fd51bcaf0ebbf3b6e875e737b58a98c993c67623
sangmi <sang1029110@gmail.com>
2025-02-20 02:34:57 +0900

    finish1

--- 2ToDolist/dolist.js
+++ 2ToDolist/dolist.js
@@ -13,16 +13,23 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let tabs =document.querySelectorAll(".task-tabs div");
+let underLine = document.getElementById("under-line")
 let taskList = [];
 let filterList =[];
 let mode='all';
 
 addButton.addEventListener("click",addTask);
+tabs.forEach(menu => menu.addEventListener("click",(event) => move(event)));
 
 for(let i = 1; i<tabs.length;i++){
   tabs[i].addEventListener("click",function(event){move(event)});
 }
 console.log(tabs)
+
+taskInput.addEventListener("focus", function () {
+  this.value = ""; // 클릭하면 기존 입력값 삭제
+});
+
 function addTask(){
   let task = {
     id:randomIDGenerate(),
@@ -35,20 +42,21 @@ function addTask(){
 }
 
 function render(){
+  let resultHTML = "";
   //1. 내가 선택한 탭에 따라서
   let list = []
   if(mode === "all"){
-    list = taskList;
-  }else if(mode === "ongoing"){
+    list = [...taskList].sort((a, b) => a.isComplete - b.isComplete);
+  }else if(mode === "ongoing" || mode === "done"){
     list = filterList;
   }
 
-  let resultHTML = "";
+
   for(let i = 0; i<list.length; i++){
     if(list[i].isComplete == true){
-      resultHTML+= `<div class = "task">
+      resultHTML+= `<div class = "task task-done">
           <div class ="task-done">${list[i].taskContent}</div>
-          <div>
+          <div class = "button-box">
             <button onclick="toggleComplete('${list[i].id}')" type="button" class="btn btn-outline-secondary"><i class="fa-solid fa-rotate-left"></i></button>
             <button onclick = "deleteTask('${list[i].id}')" type="button" class="btn btn-info">Delete</button>
           </div>
@@ -95,6 +103,15 @@ function deleteTask(id){
 }
 
 function move(event){
+  if(event){
+    mode = event.target.id;
+    underLine.style.left = event.target.offsetLeft + "px";
+    underLine.style.width = event.target.offsetWidth + "px";
+    underLine.style.top = event.target.offsetTop + (event.target.offsetHeight - 4) + "px" ;
+    render();
+  }// 마지막 줄 코드: 진행중 상태에서 끝남으로 표시하면 바로 사라지는 부분은 event
+
+
   mode = event.target.id;
   filterList =[];
   if(mode ==="all"){
@@ -108,10 +125,15 @@ function move(event){
     }
     render();
   }else if(mode === "done"){
-    //task.isComplete = true
-
+    for(let i = 0; i<taskList.length; i++){
+      if(taskList[i].isComplete === true){
+        filterList.push(taskList[i]);
+      }
+    }
+  render();
   }
 }
+
 
 function randomIDGenerate(){
   return '_' + Math.random().toString(36).slice(2, 16);
