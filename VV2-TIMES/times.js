586919b0b701bfdd477a5b278f3a812e7ba3bccd
sangmi <sang1029110@gmail.com>
2025-02-25 23:14:04 +0900

    VV2-TIMES1

---
+++ VV2-TIMES/times.js
@@ -0,0 +1,10 @@
+const API_KEY = `5ef9d02cad9949e1af0a1b501925f601`
+let news = []
+const getLatestNews = async() => {
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`)
+  const response = await fetch(url)
+  const data = await response.json()
+  news = data.articles
+  console.log("ddd",news)
+}
+getLatestNews()
