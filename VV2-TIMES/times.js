f25f650612a4fad86e6faa0c29ed8c37f7641cc4
sangmi <sang1029110@gmail.com>
2025-02-27 03:02:14 +0900

    add newsList

--- VV2-TIMES/times.js
+++ VV2-TIMES/times.js
@@ -1,10 +1,40 @@
 const API_KEY = `5ef9d02cad9949e1af0a1b501925f601`
-let news = []
+let newList = []
 const getLatestNews = async() => {
   const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?page=1&pageSize=20&country=kr&apiKey=${API_KEY}`)
   const response = await fetch(url)
   const data = await response.json()
-  news = data.articles
-  console.log("ddd",news)
+  newList = data.articles
+  render()
+  console.log("ddd",newList)
 }
+
+const render =() =>{
+
+  const newsHTML = newList.map(news => `     
+    <div class = "row news ">
+      <div class = "col-lg-4 ">
+        <img class="news-img-size" src="${news.urlToImage ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'}"
+        // onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';" />
+
+      </div>
+      <div class = "col-lg-8 news-content">
+        <h2>${news.title}</h2>
+        <p>
+          ${news.description 
+            ? news.description.length > 200 
+              ? news.description.substring(0, 200) + "..."
+              : news.description 
+            : "내용없음"}
+        </p>
+        <div>
+          ${news.source.name || "no source"} *${moment(news.publishedAt).fromNow()}
+        </div>
+      </div>
+    </div>` ).join('')
+  
+  
+  document.getElementById('news-board').innerHTML = newsHTML
+}
+
 getLatestNews()
